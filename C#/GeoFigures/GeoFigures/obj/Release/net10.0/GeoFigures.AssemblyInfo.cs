dcfd1f97332a77b2deab931bc1ffe5e8d15c9b8d
Lukas <lukasberzis2@gmail.com>
2026-04-20 20:04:32 +0300

    test
    test

--- C#/GeoFigures/GeoFigures/obj/Release/net10.0/GeoFigures.AssemblyInfo.cs
+++ C#/GeoFigures/GeoFigures/obj/Release/net10.0/GeoFigures.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeoFigures")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b2182e9ac646028b096a714f4a216189cd959e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeoFigures")]
