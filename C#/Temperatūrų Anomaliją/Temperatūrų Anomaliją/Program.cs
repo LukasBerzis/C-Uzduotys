7b5841862cfcacb1b560259802de137ccc3485f7
Lukas <lukasberzis2@gmail.com>
2026-04-24 13:04:05 +0300

    Baigti praktiniai uzdaviniai

---
+++ C#/Temperatūrų Anomaliją/Temperatūrų Anomaliją/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Temperatūros_anomalija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double[] Temperature = new double[30];
+
+            double avg = 0;
+
+            for (int i = 0; i < Temperature.Length; i++)
+            {
+                Temperature[i] = Math.Round(random.NextDouble() * (36 - (-10)) + (-10), 1);
+            }
+
+            avg = Math.Round(Temperature.Average(), 1);
+
+            Console.WriteLine($"Temperaturos Vidurkis: {avg} C");
+
+            Console.WriteLine("Pradinis masyvas: ");
+            foreach (var item in Temperature)
+            {
+                Console.Write($" {item} C ");
+            }
+
+            int diena = 1;
+            Console.WriteLine("Temperaturos anomalijos: ");
+            foreach (var item in Temperature)
+            {
+                int i = 1;
+                double temp = Math.Round(item, 0);
+                if (Math.Abs(temp - avg) >= 8)
+                {
+                    Console.WriteLine($"Temperaturos anomalijs - {item} C  {diena} diena");
+                }
+
+                diena++;
+            }
+
+
+        }
+    }
+}
---
+++ C#/mokinių vardus/mokinių vardus/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<string, int> MokiniuSarasas = new Dictionary<string, int>();
+
+           Console.WriteLine("iveskitem mokinio varda ir balus (Aldona 95)(palikite tuscia kad iseiti): ");
+
+            while (true)
+            {
+                string Mokinys = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(Mokinys))
+                {
+                    break;
+                }
+
+                string[] split = Mokinys.Split(' ');
+
+                MokiniuSarasas.Add(split[0], Convert.ToInt32(split[1]));
+            }
+
+
+            using (StreamWriter sw = new StreamWriter("students.txt", true) )
+            {
+                foreach (var item in MokiniuSarasas)
+                {
+                    sw.WriteLine($"{item.Key}, {item.Value}");
+                }
+            }
+
+            List<(string vardas, int balas)> list = new List<(string, int)>();
+
+            using (StreamReader sr = new StreamReader("students.txt", true))
+            {
+                string eilute;
+
+                while ((eilute = sr.ReadLine()) != null)
+                {
+                    string[] split = eilute.Split(',');
+
+                    string vardas = split[0];
+                    int balas = Convert.ToInt32(split[1]);
+
+                    list.Add((vardas, balas));
+                }
+            }
+
+            var sort = list.OrderByDescending(x => x.balas);
+
+            Console.WriteLine("Surusiuoti mokiniai");
+
+            foreach (var item in sort)
+            {
+                Console.WriteLine($"{item.vardas} -> {item.balas}");
+            }
+        }
+    }
+}
